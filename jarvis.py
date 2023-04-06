bfb2785420a1abec385e9b45a0396afb72a334fb
MARIANA_ALBERTO.prog/ <mariana_alberto@live.com.pt>
2023-04-06 09:19:06 +0100

    Remove ignored files

---
+++ jarvis.py
@@ -0,0 +1,20 @@
+# run in bash >>>>> $ python jarvis.py
+
+import pyttsx3
+
+# Create the object engine to initialize the text-to-speech
+engine = pyttsx3.init()
+
+
+
+# Set properties for the voice
+voice = engine.getProperty('voices')[0]                                                                                 # Get the voice object for TTS_MS_EN-US_DAVID_11.0
+engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0')       # Set the voice for the engine to use
+engine.setProperty('rate', 170)                                                                                         # Set the rate of speech (words per minute)
+engine.setProperty('volume', 1.0)                                                                                       # Set the volume (between 0 and 1)
+engine.setProperty('pitch', 80)                                                                                         # Set the pitch of the voice to a lower value (entoação/tom de voz)
+
+# Test the configured voice
+engine.say('This is JARVIS')
+engine.runAndWait()
+
